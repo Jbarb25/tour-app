e72cd91799c63686093b738453d92d658ace375e
Janaina Barboza <barboza205@usf.edu>
2025-04-11 14:36:55 -0400

    Setup root component with state and props structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/Styles.css'
+import React, {useState} from 'react';
+import Gallery from './components/Gallery.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const [tour, setTours] = useState([]) //holds all tours we want to hold
+  
+  const tourRemoval = (id) => {
+    setTours((prevTours) => prevTours.filter((tour) => tour.id !== id)); //removed the tour with the specific link 
+  };
 
+  //render the tours to the page
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main> 
+      <h1>Gallery of Tours</h1>
+      <Gallery tour={tour} setTours={setTours} onRemove={tourRemoval} /> 
+
+    </main>
   )
 }
 
